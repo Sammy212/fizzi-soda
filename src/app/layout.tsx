3cb3537e97e2a8a38e04855f0bcd86bfa1988f4c
Samuel Afolabi <samuelafo.212@gmail.com>
2025-01-01 11:25:15 +0100

    Added Hero Slice

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,30 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
+import localFont from "next/font/local";
+import "./app.css";
+import Header from "@/components/Header";
+
+const alpino = localFont({
+  src: "../../public/fonts/alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={alpino.variable}>
+      <body className="overflow-x-hidden bg-yellow-300">
+        <Header/>
+        <main>
+          {children}
+        </main>
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
