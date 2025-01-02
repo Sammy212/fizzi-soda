a10034bb59fa13503a8dbc7790096b269d036f2d
Samuel Afolabi <samuelafo.212@gmail.com>
2025-01-02 03:16:56 +0100

    Updated NEXT Version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
+import localFont from "next/font/local";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
-import localFont from "next/font/local";
 import "./app.css";
 import Header from "@/components/Header";
+import ViewCanvas from "@/components/ViewCanvas";
 
 const alpino = localFont({
-  src: "../../public/fonts/alpino-Variable.woff2",
+  src: "../../public/fonts/Alpino-Variable.woff2",
   display: "swap",
   weight: "100 900",
   variable: "--font-alpino",
-})
+});
 
 export default function RootLayout({
   children,
@@ -20,9 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={alpino.variable}>
       <body className="overflow-x-hidden bg-yellow-300">
-        <Header/>
+        <Header />
         <main>
           {children}
+          <ViewCanvas />
         </main>
       </body>
       <PrismicPreview repositoryName={repositoryName} />
