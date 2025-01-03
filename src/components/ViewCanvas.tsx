fc9020f6109408e74f5cc2162d8c2b82d5179671
Samuel Afolabi <samuelafo.212@gmail.com>
2025-01-03 11:18:43 +0100

    Updated Hero | Update Slice Sim

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -1,34 +1,45 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { Environment, Float, View } from "@react-three/drei";
-import FloatingCan from "@/components/FloatingCan";
-import Scene from "@/slices/Hero/Scene";
+import { View } from "@react-three/drei";
 import {Perf} from "r3f-perf"
+import { Suspense } from "react";
+
+import dynamic from "next/dynamic";
+
+const Loader = dynamic(
+  () => import("@react-three/drei").then((mod) => mod.Loader), 
+    { ssr: false }
+)
 
 type Props = {};
 
 export default function ViewCanvas({}: Props) {
   return (
-    <Canvas
-      style={{
-        position: "fixed",
-        top: 0,
-        left: "50%",
-        transform: "translateX(-50%)",
-        overflow: "hidden",
-        pointerEvents: "none",
-        zIndex: 30,
-      }}
-      shadows
-      dpr={[1, 1.5]}
-      gl={{ antialias: true }}
-      camera={{
-        fov: 30,
-      }}
-    >
-      <View.Port />
-      {/* <Perf /> */}
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          overflow: "hidden",
+          pointerEvents: "none",
+          zIndex: 30,
+        }}
+        shadows
+        dpr={[1, 1.5]}
+        gl={{ antialias: true }}
+        camera={{
+          fov: 30,
+        }}
+      >
+        <Suspense fallback={null}>
+          <View.Port />
+          {/* <Perf /> */}
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 }
